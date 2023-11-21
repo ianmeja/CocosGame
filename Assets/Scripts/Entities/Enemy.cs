38acde2136552fbc7761eb5fc5141596ef5cc109
joassaff <jassaff@itba.edu.ar>
2023-11-20 22:20:43 -0300

    colisiones, zombie
    colisiones auto y animacion zombie

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -11,37 +11,74 @@ public class Enemy : MonoBehaviour
     public Quaternion angle;
     public float degree;
 
+    public GameObject target;
+    public bool attack;
+
 
     // Start is called before the first frame update
     void Start()
     {
         ani = GetComponent<Animator>();
+        target = GameObject.Find("TT_demo_police");
     }
 
     void behaviour_enemy()
     {
-        chronometer += 1 * Time.deltaTime;
-        if (chronometer >= 4)
+        if (target == null)
         {
-            routine = Random.Range(0, 2);
-            chronometer = 0;
+            Debug.LogError("La variable 'target' no ha sido asignada en el Inspector.");
+            return; // Sale de la función para evitar errores adicionales.
         }
-        switch (routine)
+        if (Vector3.Distance(transform.position, target.transform.position) > 5){
+
+            ani.SetBool("run", false);
+            chronometer += 1 * Time.deltaTime;
+            if (chronometer >= 4)
+            {
+                routine = Random.Range(0, 2);
+                chronometer = 0;
+            }
+            switch (routine)
+            {
+                case 0:
+                    ani.SetBool("walk", false);
+                    break;
+                case 1:
+                    degree = Random.Range(0, 360);
+                    angle = Quaternion.Euler(0, degree, 0);
+                    routine++;
+                    break;
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, 0.5f);
+                    transform.Translate(Vector3.forward * 0.3f * Time.deltaTime);
+                    ani.SetBool("walk", true);
+                    break;
+            }
+        }
+        else
         {
-            case 0:
+            if (Vector3.Distance(transform.position, target.transform.position) > 1 && !attack){
+                var lookPos = target.transform.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
                 ani.SetBool("walk", false);
-                break;
-            case 1:
-                degree = Random.Range(0, 360);
-                angle = Quaternion.Euler(0, degree, 0);
-                routine++;
-                break;
-            case 2:
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, 0.5f);
-                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
-                ani.SetBool("walk", true);
-                break;
+                ani.SetBool("run", true);
+                transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+                ani.SetBool("attack", false);
+            }
+            else{
+                ani.SetBool("walk", false);
+                ani.SetBool("run", false);
+                ani.SetBool("attack", true);
+                attack = true;
+            }
         }
+    }
+
+    public void Final_Ani(){
+        ani.SetBool("attack", false);
+        attack = false;
     }
 
     // Update is called once per frame
