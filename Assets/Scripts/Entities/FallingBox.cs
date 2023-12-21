f3b29a1d07a1b1363159183739ff21e8bb4996e0
Justina Vacas Castro <juvacas@itba.edu.ar>
2023-12-21 14:07:28 -0300

    arreglo policia en nivel 2 + agrego de cajas que caen

--- Assets/Scripts/Entities/FallingBox.cs
+++ Assets/Scripts/Entities/FallingBox.cs
@@ -23,7 +23,7 @@ public class FallingBox : MonoBehaviour
         RaycastHit hitInfo;
         // Ray es la info basica del rayo a disparar
         Ray ray = new Ray(transform.position, Vector3.down);
-        Debug.DrawRay(transform.localPosition, Vector3.down * _minHeightDetection, Color.red);
+        // Debug.DrawRay(transform.localPosition, Vector3.down * _minHeightDetection, Color.red);
         // Pregunta si se detectan colisiones
         if (!_parachuteDeployed && Physics.Raycast(ray, out hitInfo, _minHeightDetection))
         {
